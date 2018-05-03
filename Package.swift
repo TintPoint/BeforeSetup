0bf04d0b5dbbf63f0ae1c2d482f069c5c8dfa17b
Justin Jia <justin.jia.developer@gmail.com>
2018-05-03 11:21:39 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
     name: "BeforeSetup",
     dependencies: [
-        .package(url: "https://github.com/apollographql/apollo-ios", from: "0.7.0"),
-        .package(url: "https://github.com/jpsim/yams", from: "0.5.0"),
+        .package(url: "https://github.com/apollographql/apollo-ios", from: "0.8.0"),
+        .package(url: "https://github.com/jpsim/yams", from: "0.7.0"),
         .package(url: "https://github.com/kishikawakatsumi/keychainaccess", from: "3.1.0")
     ],
     targets: [
